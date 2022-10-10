0b99d96d78b4cf265c66f5622e4d60426860b7b5
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 10:05:15 -0700

    Assignment 7-6

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
-# ******************************
-# Make your Code
-# ******************************
+if len(num1) < len(num2):
+    num3 = num1 + num2
+else:
+    num3 = num2 + num1
 
-# print (num3) 
+print(num3)
